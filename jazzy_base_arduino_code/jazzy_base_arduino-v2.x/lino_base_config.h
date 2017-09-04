41072cc83d3ee48d4f5f15debcacb3e0244d1724
ebrand0007 <ebrand@edslt3.area52.com>
2017-09-04 07:49:59 -0400

    working 2.1.0 code (stripped bare min)

--- jazzy_base_arduino_code/jazzy_base_arduino-v2.x/lino_base_config.h
+++ jazzy_base_arduino_code/jazzy_base_arduino-v2.x/lino_base_config.h
@@ -19,41 +19,11 @@
 
 
 //define your robot base's specs here
-//defaults
-//#define wheel_diameter 0.069 //wheel's diameter in meters
-//#define wheel_width 0.027 //wheel's width in meters
-//#define track_width 0.22 // width of the plate you are using
-
 #define wheel_diameter 0.2286 //wheel's diameter in meters 9"
 #define wheel_width 0.0762 //wheel's width in meters 3"
 #define track_width 0.5715 // width of the plate you are using - 22.5"?? between tires
 
 
-//arduino intrupt pins:
-//https://www.arduino.cc/en/Reference/AttachInterrupt
-//Board                                   Digital Pins Usable For Interrupts
-//Uno, Nano, Mini, other 328-based        2, 3
-//Mega, Mega2560, MegaADK                 2, 3, 18, 19, 20, 21
-//Micro, Leonardo, other 32u4-based       0, 1, 2, 3, 7
-//Zero                                    all digital pins, except 4
-//MKR1000 Rev.1                           0, 1, 4, 5, 6, 7, 8, 9, A1, A2
-//Due                                     all digital pins
-//teensey                                 all digital pins
-//don't change this if you followed the schematic diagram
-//ENCODER PINS
-#define left_encoder_a 19 //interupt for left encoder
-#define left_encoder_b 18 //interupt for left encoder
-#define right_encoder_a 3 //interupt for right encoder 
-#define right_encoder_b 2 //interupt for right encoder
 
-//don't change this if you followed the schematic diagram
-//MOTOR PINS
-//Left Motor
-#define left_motor_direction 7
-#define left_motor_pwm 8
-
-//Right Motor
-#define right_motor_direction 9
-#define right_motor_pwm 10
 
 #endif
